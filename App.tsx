b1802cfac1dcae9558e2751a25e62a4bd12fbcf9
Prajwaltechversant <168541974+Prajwaltechversant@users.noreply.github.com>
2025-09-19 14:05:51 +0530

    bg colour

--- App.tsx
+++ App.tsx
@@ -61,7 +61,7 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent:'center',alignItems:'center'
+    justifyContent:'center',alignItems:'center', backgroundColor:'green'
   },
 });
 
